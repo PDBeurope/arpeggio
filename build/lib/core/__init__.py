2cfb116360df102a7eb52925bccb84b9acd5dc75
“roshan” <roshan@ebi.ac.uk>
2023-02-17 17:35:54 +0000

    updated the directory structure

--- build/lib/core/__init__.py
+++
@@ -1,3 +0,0 @@
-from arpeggio.core.exceptions import (HydrogenError, OBBioMatchError, AtomSerialError,
-                                      SiftMatchError, SelectionError)
-from arpeggio.core.interactions import InteractionComplex
